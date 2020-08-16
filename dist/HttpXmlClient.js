d2ae9f4961ea49878830d7bf53fdff0cf20fc3d8
Alexandru Cancescu <anduhd@gmail.com>
2020-08-16 17:03:48 +0300

    files naming

--- dist/HttpXmlClient.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class HttpXmlClient {
-}
-exports.default = HttpXmlClient;
-//# sourceMappingURL=HttpXmlClient.js.map
--- src/HttpXmlClient.ts
+++
@@ -1,6 +0,0 @@
-import * as xml from "fast-xml-parser"
-import * as phin from "phin"
-
-export default class HttpXmlClient {
-
-}
