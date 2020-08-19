d8a0d8254052be9fd2baafcd5b89819cb9c6d217
Alexandru Cancescu <anduhd@gmail.com>
2020-08-19 20:32:15 +0300

    increased timeout

--- dist/src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Client_1 = require("./Client");
-exports.Client = Client_1.default;
-//# sourceMappingURL=index.js.map
